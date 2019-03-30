1554281bc0e7faad7c925c4611a9b74f521c9367
Levashov, Nikita <nikita.levashov@cengage.com>
2019-03-30 19:02:55 +0200

    Add jupyther notebooks

--- neo4j_procure.py
+++ neo4j_procure.py
@@ -1,8 +1,10 @@
 from neo4j import GraphDatabase, basic_auth
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
 import pickle as pcl
+import numpy as np
 
 db_location = "bolt://localhost:7687"
 username = "neo4j"
@@ -20,39 +22,35 @@ def get_dataset(tx):
     return training_data
 
 
-def categorize(df, columns):
-    for c in columns:
-        df[c] = df[c].astype("category").cat.codes
-    return df
-
-
 def is_tobacco_of(tobacco: str):
     def go(val: str):
         return tobacco == val
     return go
 
 
+def predict(model: LogisticRegression, encoder: OrdinalEncoder, df: pd.DataFrame):
+    return model.predict(np.column_stack((encoder.transform(df.filter(items=['state_name', 'gender'])), df['age'].values)))
+
+
 def main():
     db = GraphDatabase.driver(db_location, auth=basic_auth(username, password))
+    feature_encoder = OrdinalEncoder()
 
     with db.session() as session:
         df = session.read_transaction(get_dataset)
-        df['tobacco'] = df['tobacco'].apply(is_tobacco_of('Smokeless Tobacco'))
-        df = categorize(df, ['state_name', 'gender'])
-        X_data = df.filter(items=['state_name', 'gender', 'age'])
-        y_data = df["tobacco"]
+        y_data = df['tobacco'].apply(is_tobacco_of('Cigar'))
+        X_data = np.column_stack((feature_encoder.fit_transform(df.filter(items=['state_name', 'gender'])), df['age'].values))
 
         X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.10, random_state=42)
         logistic_model = LogisticRegression(solver='lbfgs')
         logistic_model.fit(X_train, y_train)
         serz = pcl.dumps(logistic_model)
-        with open('logistic_model', 'wb') as out:
+        with open('./models/logistic_model', 'wb') as out:
             print("Serializing model to the file.. ")
             out.write(serz)
 
         y_pred = logistic_model.predict(X_test)
         res = len(y_pred[y_pred == y_test]) / len(y_pred)
         print(f"Accuracy: {res}")
-
 
 main()
